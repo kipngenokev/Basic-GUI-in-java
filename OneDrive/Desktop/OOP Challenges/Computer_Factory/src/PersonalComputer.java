3427fbea5793be8f3b5e5a8d47f99cdbf81c28cd
Kipngeno <kipngenokevin517@gmail.com>
2024-02-20 12:28:21 +0300

    Computer Factory Challenge

---
+++ OneDrive/Desktop/OOP Challenges/Computer_Factory/src/PersonalComputer.java
@@ -0,0 +1,33 @@
+public class PersonalComputer extends Product{
+    public ComputerCase computerCase;
+    public Monitor monitor;
+    public Motherboard motherboard;
+
+    public PersonalComputer(String model,String manufacturer,ComputerCase computerCase,Monitor monitor,Motherboard motherboard) {
+        super(model,manufacturer);
+        this.computerCase=computerCase;
+        this.monitor=monitor;
+        this.motherboard=motherboard;
+    }
+
+//    public ComputerCase getComputerCase() {
+//        return computerCase;
+//    }
+//
+//    public Monitor getMonitor() {
+//        return monitor;
+//    }
+//
+//    public Motherboard getMotherboard() {
+//        return motherboard;
+//    }
+
+    private void drawLogo() {
+        monitor.drawPixelAt(1200,50,"yellow");
+    }
+
+    public void powerUp() {
+        computerCase.pressPowerButton();
+        drawLogo();
+    }
+}
